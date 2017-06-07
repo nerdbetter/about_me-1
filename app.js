c14158d94611cfcd31faa61c10d5941b3865149d
ByteMiBro <bytemibro@gmail.com>
2017-06-07 14:56:15 -0600

    git mistake

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log("Loaded app.js")
+console.log('Loaded app.js');
+var userName = prompt('Type in your name');
+
+if (userName === null);
+{
+  alert('If you don\'t tell me your name, I will not know how to address you');
+  prompt(userName);
+}
+else
+{
+alert('Hi,'+' '+userName);
+}
+
+//use listor matrix to store expected answers
+// write if else in a function or loop
+//console log prints to console not browser
+//could use a do while loop to prompt user imput and then use that to continue in loop
+//poke user answer back into page using git id
+//for every question a variable to hold a value and value
